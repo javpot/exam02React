76e881723de54c2cbb61a895084024b23c9d1b42
javpot <vernetjaven@gmail.com>
2024-03-20 16:13:40 -0400

    commit exam

---
+++ ios/idServiceManager.m
@@ -0,0 +1,8 @@
+//
+//  idServiceManager.m
+//  exam02React
+//
+//  Created by Javen Vernet on 2024-03-20.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/idServiceModule.h
@@ -0,0 +1,12 @@
+//
+//  idServiceModule.h
+//  exam02React
+//
+//  Created by Javen Vernet on 2024-03-20.
+//
+
+#ifndef idServiceModule_h
+#define idServiceModule_h
+
+
+#endif /* idServiceModule_h */
